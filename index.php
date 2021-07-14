fda12553933bf7a0c5849e8d754d74447510d230
sanjoli63 <sanjoligo6mar@gmail.com>
2021-07-14 14:24:32 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+$_Session;
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>My website</title>
+    </head>
+    <body>
+        <a href="logout.php">Logout</a>
+        <h1> THIS IS THE INDEX PAGE</h1>
+        <br>
+        Hello,username
+    </body>
+</html>
